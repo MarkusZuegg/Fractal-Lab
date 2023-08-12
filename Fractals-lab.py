5b27eaa5874d89aad25a8b5c6f0b432244daba02
MarkusZuegg <markuszuegg@outlook.com>
2023-08-12 23:02:00 +1000

    started working on Ikeda map

--- Fractals-lab.py
+++ Fractals-lab.py
@@ -4,6 +4,7 @@ This is for the 3rd part. Creating and coding my own fractal set
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+import random
 
 def processFractal(a):
     """Display an array of iteration counts as a colorful picture of a fractal."""
@@ -14,22 +15,47 @@ def processFractal(a):
     a = np.uint8(np.clip(a, 0, 255))
     return a
 
-def create_mgrid(i_l, i_u, r_l, r_u, res):
-    """creates two 2D arrays given upper and lower limits"""
-    # modify to use linspace for accurate res
-    R_ = torch.linspace(r_l,r_u, res)
-    I_ = torch.linspace(i_l,i_u, res)
-    I, R = torch.meshgrid(I_, R_)
-    return I, R
-
 def draw_fractal(array):
     fig = plt.figure(figsize=(16,10))
     plt.imshow(processFractal(array.numpy()))
     plt.tight_layout(pad=0)
     plt.show()
 
+def inti_points(num_points, r):
+    """Create two tensors with all the x values and y values of points
+        num_points: int
+        r: int of the range of x and y"""
+    list_x = []
+    list_y = []
+    for i in range(num_points):
+        p_x = random.uniform(-r, r)
+        list_x.append(p_x)
+        p_y = random.uniform(-r, r)
+        list_y.append(p_y)
+    return list_x,list_y
+
+def ikeda_map(u, list_x, list_y, max_iterations, device):
+    #setup variables
+    x = torch.FloatTensor(list_x)
+    y = torch.FloatTensor(list_y)
+    t_n = torch.zeros_like(x)
+
+    #Move variables to gpu
+    x = x.to(device)
+    y = y.to(device)
+    t_n = t_n.to(device)
+
+    for i in range(max_iterations):
+        t_n = 0.4 -(6/(1 + x**2 + y**2))
+        x_ = 1 + u*(x*np.cos(t_n)-y*np.sin(t_n))
+        y_ = u*(x*np.sin(t_n)+y*np.cos(t_n))
+        x = x_
+        y = y_
+    return
+
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    return
+    
+    p = inti_points(5, 10)
 
 if __name__ == '__main__': main()
